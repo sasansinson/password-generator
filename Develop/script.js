8a12b60c402b95b02b7f4ea8762ca7b7cb57e82b
sasan sahami <sasan_2002@yahoo.com>
2021-12-18 19:35:55 +1100

    final commit

--- Develop/script.js
+++ Develop/script.js
@@ -25,33 +25,125 @@ function generatePassword(){
   
   
   //Employee choices are taken here
+  //Next 4 Ifs are adding one character to the password if employee answers YES.
   number = confirm("Do you want number in your password?");
+  if (number){
+    passwordArray = numberOptions;
+    password += passwordArray[Math.floor(Math.random() * passwordArray.length)]}
   upperCase = confirm("Do you want uppercase letter in your password?")
+  if (upperCase){
+    passwordArray = upperCaseOptions;
+    password += passwordArray[Math.floor(Math.random() * passwordArray.length)]}
   lowerCase = confirm("Do you want lowercase letter in your password?")
+  if (lowerCase){
+    passwordArray = lowerCaseOptions;
+    password += passwordArray[Math.floor(Math.random() * passwordArray.length)]}
   specialCharacter = confirm("Do you want special character in your password?")
-  
+  if (specialCharacter){
+    passwordArray = specialCharacterOptions;
+    password += passwordArray[Math.floor(Math.random() * passwordArray.length)]}
+
+
+  //This loop is for when all criterias are selected
   if (number && upperCase && lowerCase && specialCharacter){
     passwordArray = numberOptions.concat(upperCaseOptions,lowerCaseOptions,specialCharacterOptions);
+    for (let i=4; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+     }
+     return (password);
+ 
+
+
   } else if (number && upperCase && lowerCase){
     passwordArray = numberOptions.concat(upperCaseOptions,lowerCaseOptions);
+   
+    //This loop is for when three criterias are selected
+    for (let i=3; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+  
   } else if (number && upperCase && specialCharacter){
     passwordArray = numberOptions.concat(upperCaseOptions,specialCharacterOptions);
+  
+    //This loop is for when three criterias are selected
+    for (let i=3; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+  
   } else if (number && lowerCase && specialCharacter){
     passwordArray = numberOptions.concat(lowerCaseOptions,specialCharacterOptions);
+  
+    //This loop is for when three criterias are selected
+    for (let i=3; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+
   } else if (upperCase && lowerCase && specialCharacter){
     passwordArray = upperCaseOptions.concat(lowerCaseOptions,specialCharacterOptions);
+  
+    //This loop is for when three criterias are selected
+    for (let i=3; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+  
   } else if (number && upperCase){
     passwordArray = numberOptions.concat(upperCaseOptions);
+    //This loop is for when two criterias are selected
+    for (let i=2; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+  
   } else if (number && lowerCase){
     passwordArray = numberOptions.concat(lowerCaseOptions);
+    
+    //This loop is for when two criterias are selected
+    for (let i=2; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+  
+  
   } else if (number && specialCharacter){
     passwordArray = numberOptions.concat(specialCharacterOptions);
+   
+    //This loop is for when two criterias are selected
+    for (let i=2; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+  
   } else if (upperCase && lowerCase){
     passwordArray = upperCaseOptions.concat(lowerCaseOptions);
+    
+    //This loop is for when two criterias are selected
+    for (let i=2; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+  
   } else if (upperCase && specialCharacter){
     passwordArray = upperCaseOptions.concat(specialCharacterOptions);
+   
+    //This loop is for when two criterias are selected
+    for (let i=2; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+  
   } else if (lowerCase && specialCharacter){
     passwordArray = lowerCaseOptions.concat(specialCharacterOptions);
+    
+    //This loop is for when two criterias are selected
+    for (let i=2; i<passwordLength;i++){
+      password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+      }
+      return (password);
+  
   } else if (number){
     passwordArray = numberOptions;
   } else if (upperCase){
@@ -66,13 +158,12 @@ function generatePassword(){
   }
 
 
-  //Loop that consolidate computer character choices and password length
-    for (let i=0; i<passwordLength;i++){
-     password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
-    }
-    return (password);
+  //This loop is for when only one criteria is selected
+  for (let i=1; i<passwordLength;i++){
+  password += passwordArray[Math.floor(Math.random() * passwordArray.length)]
+  }
+  return (password); 
 }
-
 
 // Write password to the #password input
 function writePassword() {
